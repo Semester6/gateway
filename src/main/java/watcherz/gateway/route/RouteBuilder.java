4254eccbb343571521702be13bffdf91db2d1428
Sharony Braam <fogsliezeta@hotmail.com>
2022-05-27 19:13:01 +0200

    disabled routelocator

--- src/main/java/watcherz/gateway/route/RouteBuilder.java
+++ src/main/java/watcherz/gateway/route/RouteBuilder.java
@@ -1,35 +1,35 @@
-package watcherz.gateway.route;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import watcherz.gateway.config.AuthenticationFilter;
-
-@Configuration
-@CrossOrigin(origins = "*")
-public class RouteBuilder {
-
-    @Autowired
-    AuthenticationFilter filter;
-
-    @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(r -> r
-                        .path("/movie/**")
-                        .filters(f -> f.filter(filter))
-                        .uri("lb://movieservice"))
+//package watcherz.gateway.route;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import watcherz.gateway.config.AuthenticationFilter;
+//
+//@Configuration
+//@CrossOrigin(origins = "*")
+//public class RouteBuilder {
+//
+//    @Autowired
+//    AuthenticationFilter filter;
+//
+//    @Bean
+//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
 //                .route(r -> r
-//                        .path("/user/**")
-//                        .filters(f -> f.addRequestHeader("something", "something"))
-//                        .uri("lb://USER-SERVICE"))
-                .route(r -> r
-                        .path("/auth/**")
-                        .filters(f -> f.filter(filter))
-                        .uri("lb://authservice"))
-                .build();
-    }
-}
+//                        .path("/movie/**")
+//                        .filters(f -> f.filter(filter))
+//                        .uri("lb://movieservice"))
+////                .route(r -> r
+////                        .path("/user/**")
+////                        .filters(f -> f.addRequestHeader("something", "something"))
+////                        .uri("lb://USER-SERVICE"))
+//                .route(r -> r
+//                        .path("/auth/**")
+//                        .filters(f -> f.filter(filter))
+//                        .uri("lb://authservice"))
+//                .build();
+//    }
+//}
