33268c2bde5a8bc89f01590078b54f33601f6a08
Sharony Braam <fogsliezeta@hotmail.com>
2022-05-27 17:20:39 +0200

    kubernet discovery test

--- src/main/java/watcherz/gateway/GatewayApplication.java
+++ src/main/java/watcherz/gateway/GatewayApplication.java
@@ -1,12 +1,17 @@
 package watcherz.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
